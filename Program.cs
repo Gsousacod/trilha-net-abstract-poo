4532319ef7f47d5cc1a532729ced195b29d0f7e9
Gabriel Sousa <gds8@aluno.ifnmg.edu.br>
2024-10-17 10:27:53 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia( "12345",  "Modelo1","11111111", 60);
+nokia.Ligar();
+nokia.InstalarAplicativo("whatsapp");
+
+Console.WriteLine("Smartphone iphone:");
+Smartphone iphone = new Iphone( "12345",  "Modelo1","11111111", 60);
+iphone.Ligar();
+iphone.InstalarAplicativo("whatsapp");
+
